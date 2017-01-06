d5963d41c99b62794a42164791c39102da12e946
hedwig34 <hedwig34@gmail.com>
2017-01-06 16:15:43 -0500

    d2tmo: Add d2tmo
    Change-Id: I7123341916d678cbe5b9e9a6b2a66a9bedf8557d

---
+++ samsung/d2tmo/to.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+$(call inherit-product, device/samsung/d2tmo/full_d2tmo.mk)
+
+# Inherit device common stuff specific to TO
+$(call inherit-product, vendor/to/product/samsung/d2-common/to.mk)
+
+# Inherit some common TO stuff.
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=d2uc \
+    TARGET_DEVICE=d2tmo \
+    BUILD_FINGERPRINT="samsung/d2tmo/d2tmo:4.3/JSS15J/T999UVUEMJC:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2tmo-user 4.3 JSS15J T999UVUEMJC release-keys"
+
+PRODUCT_NAME := to_d2tmo
+PRODUCT_DEVICE := d2tmo
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 add_lunch_combo to_athene-userdebug
 add_lunch_combo to_d2att-userdebug
 add_lunch_combo to_d2spr-userdebug
+add_lunch_combo to_d2tmo-userdebug
 add_lunch_combo to_d2vzw-userdebug
 add_lunch_combo to_jflteatt-userdebug
 add_lunch_combo to_oneplus3-userdebug
