e1caafb3d4a046bdd8cd1c6a0a4a7487e362d09e
John <manfromgta@gmail.com>
2017-01-07 14:47:49 -0500

    S4-jfltexx Bring Up
    Change-Id: I59829cd7126e5ff548054bfb594c958f74618595

---
+++ samsung/jfltexx/to.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+$(call inherit-product, device/samsung/jfltexx/full_jfltexx.mk)
+
+# Inherit device common stuff specific to TO
+$(call inherit-product, vendor/to/product/samsung/jf-common/to.mk)
+
+# Inherit some common TO stuff.
+$(call inherit-product, vendor/to/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jfltexx \
+  TARGET_DEVICE=jfltexx \
+  BUILD_FINGERPRINT=samsung/jfltexx/jflte:5.0.1/LRX22C/I9505XXUHPF4:user/release-keys \
+  PRIVATE_BUILD_DESC="jfltexx-user 5.0.1 LRX22C I9505XXUHPF4 release-keys"
+
+PRODUCT_DEVICE := jfltexx
+PRODUCT_NAME := to_jfltexx
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo to_d2spr-userdebug
 add_lunch_combo to_d2tmo-userdebug
 add_lunch_combo to_d2vzw-userdebug
 add_lunch_combo to_jflteatt-userdebug
+add_lunch_combo to_jfltexx-userdebug
 add_lunch_combo to_oneplus3-userdebug
 add_lunch_combo to_Z00A-userdebug
 add_lunch_combo to_Z008-userdebug
