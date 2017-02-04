08ebef57f9206372f969eaafcc9f1d73546b8a53
John <manfromgta@gmail.com>
2017-02-04 10:39:33 -0500

    Alphabetize Devices
    Change-Id: I817bff676058c870113a994a8120100a4910fe2e

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,9 +13,9 @@ add_lunch_combo to_jfltespr-userdebug
 add_lunch_combo to_jfltexx-userdebug
 add_lunch_combo to_ls990-userdebug
 add_lunch_combo to_oneplus3-userdebug
-add_lunch_combo to_vs985-userdebug
+add_lunch_combo to_s2-userdebug
 add_lunch_combo to_serranolteusc-userdebug
+add_lunch_combo to_vs985-userdebug
 add_lunch_combo to_Z00A-userdebug
 add_lunch_combo to_Z00T-userdebug
 add_lunch_combo to_Z008-userdebug
-add_lunch_combo to_s2-userdebug
