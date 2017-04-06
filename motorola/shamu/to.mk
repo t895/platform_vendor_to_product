b759d9699684afa1f0439111859c12aae9b9840e
cjkacz <cjkacz@gmail.com>
2017-04-05 23:14:43 -0400

    Shamu: add Uber toolchains
    Change-Id: I2d4c58a1424ac39b6fa5e176ddb897359a12c139

--- motorola/shamu/to.mk
+++ motorola/shamu/to.mk
@@ -18,6 +18,12 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 #Device overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/to/product/motorola/shamu/overlay
 
+# Build using uber toolchain on linux but use default toolchain on darwin.
+ifneq ($(HOST_OS),darwin)
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9-uber/bin
+TARGET_GCC_VERSION_EXP := 4.9-uber
+endif
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
