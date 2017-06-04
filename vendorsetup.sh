2ca84e3a65ce356a71b80a6d7bdae0455101b29b
Adrian Quintanilla Martinez <adri16072002@gmail.com>
2017-06-04 09:24:31 -0400

    Add cedric (Moto G5)
    Change-Id: I3575283a545ab031f409391f28b54311edeff9da

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,7 @@ add_lunch_combo to_armani-userdebug
 add_lunch_combo to_athene-userdebug
 add_lunch_combo to_bullhead-userdebug
 add_lunch_combo to_cancro-userdebug
+add_lunch_combo to_cedric-userdebug
 add_lunch_combo to_d2att-userdebug
 add_lunch_combo to_d2spr-userdebug
 add_lunch_combo to_d2tmo-userdebug
