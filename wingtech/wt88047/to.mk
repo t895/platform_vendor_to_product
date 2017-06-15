d75e095b13565df773271bbe2a0280ba3f7cae1e
aldi prasetyo <aldi.45ckr@gmail.com>
2017-06-15 19:54:50 -0400

    wt88047: Maintainer takeover
    * Change location to wingtech
    * Change remote device tree and vendor blob to my github
    
    Change-Id: Ib8c506819357903806630fe648bfd2fea6590e55

--- wingtech/wt88047/to.mk
+++ wingtech/wt88047/to.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2015-2016, The CyanogenMod Project
+# Copyright (C) 2017, The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,14 +15,14 @@
 
 $(call inherit-product, device/wingtech/wt88047/full_wt88047.mk)
 
-# Inherit some common LineageOS stuff.
+# Device overlay
+DEVICE_PACKAGE_OVERLAYS += vendor/to/product/wingtech/wt88047/overlay
+
+# Inherit some common Lineage stuff.
 $(call inherit-product, vendor/to/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
-
-# Hardcode the path to overlays
-DEVICE_PACKAGE_OVERLAYS += vendor/to/product/wingtech/wt88047/overlay
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := HM2014811,HM2014812,HM2014813,HM2014814,HM2014815,HM2014816,HM2014817,HM2014818,HM2014819,HM2014820,HM2014821,HM2014112,wt88047,wt86047
@@ -32,6 +33,6 @@ PRODUCT_DEVICE := wt88047
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Specify (Lets be on safer side)
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Xiaomi/wt88047/wt88047:5.1.1/LMY47V/V8.2.1.0.LHJMIDL:user/release-keys \
+    PRIVATE_BUILD_DESC="wt88047-user 5.1.1 LMY47V V8.2.1.0.LHJMIDL release-keys"
