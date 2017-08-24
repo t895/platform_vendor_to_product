05de622e6b93bdeb5c9301b0bcd2ea804a4f7cc3
t895 <clombardo169@gmail.com>
2017-08-23 21:29:23 -0400

    Add clark

--- vendorsetup_community.sh
+++ vendorsetup_community.sh
@@ -1,5 +1,5 @@
 add_lunch_combo to_bacon-userdebug
-add_lunch_combo to_mark-userdebug
+add_lunch_combo to_clark-userdebug
 add_lunch_combo to_gemini-userdebug
 add_lunch_combo to_grouper-userdebug
 add_lunch_combo to_i605-userdebug
